77fea0bedb245e31cf320b566f8ff02c4bfbd498
moududulhaque <moududulhaque@gmail.com>
2025-05-04 13:40:49 +0600

    Logo Area Design done and jQuery Slick Slider Implement Done

--- src/js/jQuery.js
+++ src/js/jQuery.js
@@ -1,9 +1,40 @@
 // jQuery code to initialize the counterUp plugin
 
 $(document).ready(function () {
+
     // This code to initialize the counterUp plugin This code will run when the document is fully loaded
     $('.counter').counterUp({
         delay: 10,
         time: 1000
     });
+
+    // Slick Carousel initialization
+    $('.center').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 5,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
 });
