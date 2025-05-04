4893e0a2baf6243fea6a9963abf66431bbf3be5d
moududulhaque <moududulhaque@gmail.com>
2025-05-04 12:31:04 +0600

    Some Fix of jQuery Plugin

---
+++ src/js/jQuery.js
@@ -0,0 +1,9 @@
+// jQuery code to initialize the counterUp plugin
+
+$(document).ready(function () {
+    // This code to initialize the counterUp plugin This code will run when the document is fully loaded
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+});
