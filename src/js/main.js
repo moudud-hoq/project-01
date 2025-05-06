98dd175927fb43473ece45a101e60072cadbbbcf
moududulhaque <moududulhaque@gmail.com>
2025-05-06 20:13:20 +0600

    Mobile Search Button fold after click the search icon

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,9 @@ const mobileMenu = document.getElementById('mobileMenu');
 toggleBtn.addEventListener('click', () => {
     mobileMenu.classList.toggle('hidden');
 });
+
+// Search Button fold
+document.getElementById('mobileSearchToggle').addEventListener('click', function () {
+    const searchBar = document.getElementById('mobileSearchBar');
+    searchBar.classList.toggle('hidden');
+});
